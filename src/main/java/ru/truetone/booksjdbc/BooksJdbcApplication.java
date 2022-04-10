b14d4da7acb8c65f8461adabc599c156aede3f94
TrueToneBY <pasblek@yandex.ru>
2022-04-10 18:00:11 +0300

    project

---
+++ src/main/java/ru/truetone/booksjdbc/BooksJdbcApplication.java
@@ -0,0 +1,13 @@
+package ru.truetone.booksjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksJdbcApplication.class, args);
+    }
+
+}
