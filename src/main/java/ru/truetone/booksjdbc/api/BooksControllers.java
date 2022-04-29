f04eba924db84ae5b4766b7588e3a07da11212e7
TrueToneBY <pasblek@yandex.ru>
2022-04-29 15:44:50 +0300

    lessons_two_Cookie_get_pos_status_200

--- src/main/java/ru/truetone/booksjdbc/api/BooksControllers.java
+++ src/main/java/ru/truetone/booksjdbc/api/BooksControllers.java
@@ -1,16 +1,26 @@
 package ru.truetone.booksjdbc.api;
 
+
+import io.swagger.v3.oas.models.headers.Header;
+import jdk.jfr.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import reactor.netty.http.server.HttpServerResponse;
 import ru.truetone.booksjdbc.model.Books;
 import ru.truetone.booksjdbc.service.BooksDAO;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -58,9 +68,81 @@ public class BooksControllers {
     }
 
     
-    @PostMapping("/echo{id}")
-    public ResponseEntity<?> echo(@PathVariable String id){
-        return new ResponseEntity<>(id,HttpStatus.CREATED);
+    @PostMapping(value = "/echo",produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<String> echo(){
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    /**
+     * записать куки
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    @GetMapping("/set-cookie")
+    public ResponseEntity<?> setCookie(HttpServletResponse response) throws IOException{
+        Cookie cookie = new Cookie("data","Come_to_the_dark_side");//создаем объект Cookie,
+        //в конструкторе указываем значения для name и value
+        cookie.setPath("/");//устанавливаем путь
+        cookie.setMaxAge(86400);//здесь устанавливается время жизни куки
+        response.addCookie(cookie);//добавляем Cookie в запрос
+        response.setContentType("text/plain");//устанавливаем контекст
+        return ResponseEntity.ok().body(HttpStatus.OK);//получилось как бы два раза статус ответа установили,
+        // выбирайте какой вариант лучше
+    }
+
+    /**
+     * Чтение Cookie
+     * @param data
+     * @return
+     */
+    @GetMapping(value = "/get-cookie")
+    public ResponseEntity<?> readCookie(@CookieValue(value = "data")String data){
+        return ResponseEntity.ok().body(data);
+    }
+
+    /**
+     * прочитать заголовки:
+     * @param headers
+     * @return
+     */
+    @GetMapping(value = "/get-headers")
+    public ResponseEntity<?> getHeaders(@RequestHeader Map<String,String> headers){//представляет заголовки ввиде мапы,
+        //где ключ это наименование заголовка, а значение мапы - это значение заголовка
+        return ResponseEntity.ok(headers);
+    }
+
+    /**
+     * //записать заголовок
+     * @return
+     */
+    @GetMapping(value = "/set-header")
+    public ResponseEntity<?> setHeader(){
+        return ResponseEntity.ok().header("name- header","value-header").body(HttpStatus.OK);
+    }
+
+    /**
+     * //еще варианты работы с заголовками
+     * @return
+     */
+    @GetMapping(value = "/set-headers")
+    public ResponseEntity<?> setHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();//создаем объект
+        //который имплементирует мапу MultiValueMap<String, String>
+        //наполняем ее парами ключ-значение
+        //можно наполнить своими заголовками через метод add
+        httpHeaders.add("customer-header", "value-header1");
+        //HttpHeaders так же предлагает большой выбор стандартных заголовков
+        //Посмотрите на них набрав в IDEA HttpHeaders.
+        httpHeaders.add(HttpHeaders.FROM, "russia");
+        //можно изменить существующий заголовок, вызвав для него сет-метод
+        httpHeaders.setDate(0);
+        //или получить значение конкретного заголовка
+        Long date = httpHeaders.getDate();
+        System.out.println(date);
+        return ResponseEntity
+                .ok().headers(httpHeaders)//здесь метод принимающий MultiValueMap<String, String>
+                .body(HttpStatus.OK);
     }
 
 
---
+++ src/main/java/ru/truetone/booksjdbc/config/MyWebApplicationInitializer.java
@@ -0,0 +1,28 @@
+package ru.truetone.booksjdbc.config;
+
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/root"};
+    }
+}
